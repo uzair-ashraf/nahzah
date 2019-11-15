a85f27592d12e264d4674ff24587dff6d85e3d18
Uzair Ashraf <uzinatorcl@live.com>
2019-11-14 21:30:08 -0800

    switched to nahzahs email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -10,9 +10,9 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "uzinatorcl@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "Srogerss1@hotmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "New Avatar Request From:  $name";
-$body = "You have received a new avatar inquiry\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
+$body = "Whatsup weirdo, you got a new avatar request\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nMessage:\n$message";
 $header = "From: noreply@nahzah.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";
 
